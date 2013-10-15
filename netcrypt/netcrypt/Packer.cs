2524b1c567eabe9d980d86aaed9ba8779b49b7fc
Yvan Janssens <yvan.janssens@agfa.com>
2013-10-15 21:08:09 +0200

    Added the netcrypt source files.

---
+++ netcrypt/netcrypt/Packer.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+using Microsoft.CSharp;
+using System.IO;
+using System.Security.Cryptography;
+using System.CodeDom.Compiler;
+
+namespace Netcrypt
+{
+    /// <summary>
+    /// Provides an easy-to-use interface to a software packing algorythm for .NET executables.
+    /// </summary>
+    public class Packer
+    {
+        /// <summary>
+        /// Packs the provided assembly
+        /// </summary>
+        /// <param name="assembly">A byte[] array containing the original assemly</param>
+        /// <returns>a byte[] array containing the packed assemnly</returns>
+        public static byte[] Pack(byte[] assembly)
+        {
+            RijndaelManaged rijndael = new RijndaelManaged();
+            rijndael.KeySize = 256;
+            rijndael.GenerateIV();
+            rijndael.GenerateKey();
+
+            string key = Convert.ToBase64String(rijndael.Key);
+            string iv = Convert.ToBase64String(rijndael.IV);
+            string app = "";
+
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, rijndael.CreateEncryptor(), CryptoStreamMode.Write);
+            cryptoStream.Write(assembly, 0, assembly.Length);
+            cryptoStream.Flush();
+            memoryStream.Seek(0, SeekOrigin.Begin);
+            app = Convert.ToBase64String(memoryStream.ToArray());
+            cryptoStream.Close();
+            memoryStream.Close();
+
+            string csharpcode = netcrypt.Properties.Resources.StubCode;
+
+            csharpcode.Replace("%KEY%", key);
+            csharpcode.Replace("%IV%", iv);
+            csharpcode.Replace("%PROGRAM%", app);
+
+            CSharpCodeProvider codeProvider = new CSharpCodeProvider();
+            ICodeCompiler compiler = codeProvider.CreateCompiler();
+
+            CompilerParameters parameters = new CompilerParameters();
+            parameters.GenerateInMemory = true;
+            parameters.GenerateExecutable = true;
+            parameters.OutputAssembly = "Packed.exe";
+
+            CompilerResults results = compiler.CompileAssemblyFromSource(parameters, csharpcode);
+
+            try {
+                FileStream fs = results.CompiledAssembly.GetFiles()[0];
+                byte[] barr  = new byte[fs.Length];
+                int r = fs.Read(barr, 0, barr.Length);
+                if (r == barr.Length)
+                {
+                    return barr;
+                }
+                else
+                {
+                    return null;
+                }
+            } catch {
+                return null;
+            }
+
+        }
+    }
+}
---
+++ netcrypt/netcrypt/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("netcrypt")]
+[assembly: AssemblyDescription("netcrypt C# library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("@friedkiwi")]
+[assembly: AssemblyProduct("netcrypt")]
+[assembly: AssemblyCopyright("Copyright © friedkiwi 2013 - Licensed under the GNU GPLv3")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b12b2ca7-ab27-4f70-906e-d94f92204759")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
